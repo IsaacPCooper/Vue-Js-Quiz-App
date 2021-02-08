b4fa12e05ea57362b2e2ccfcf62f385f740924a8
Isaac Cooper <jordan.p.c.austin@gmail.com>
2021-02-08 14:35:25 +0000

    Big commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-     <h1>Question Box </h1>
+<div>
+  <b-nav tabs>
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/5</b-nav-item>
+  </b-nav>
+</div>
 </template>
